831f84a1c79100a3cf12eae79344225dc55408be
Simon Viegas <simon.viegas@outlook.com.br>
2020-08-31 13:45:05 -0300

    Gif loading + Correção CSS Footer

--- src/App.js
+++ src/App.js
@@ -63,6 +63,13 @@ export default () => {
         Direitos de imagem para Netflix<br />
         Dados pegos do site Themoviedb.org
       </footer>
+      
+      {movieList <=0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif?x91507" alt="Carregando"/>
+        </div>
+      }
+
     </div>
   )
 }
